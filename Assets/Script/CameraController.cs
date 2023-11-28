ebf33b8fb251de125ecda1fce1c33739dc74d381
TravisJon <mhdyoga1028@gmail.com>
2023-11-28 16:49:20 +0700

    perubahan

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,27 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
-    public float smoothspeed;
-    public Vector3 offset;
-
-    void Update()
-    {
-        transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, -0f, 19.8f),
-            Mathf.Clamp(transform.position.y, -0f, 0f),
-            transform.position.z);
-    }
+    [SerializeField] private Transform player;
 
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 positioncamera = target.localPosition + offset;
-        Vector3 smoothCamera = Vector3.Lerp(transform.position, positioncamera, smoothspeed);
-        transform.position = smoothCamera;
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
